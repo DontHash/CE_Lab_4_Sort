891bf110cce34f5c3491f63d73838c768dbf7104
Swarnim Bajracharya <140135866+SwarnimBajra@users.noreply.github.com>
2024-06-15 11:56:31 +0545

    Update graphInsertion.py

--- graphInsertion.py
+++ graphInsertion.py
@@ -1,8 +1,7 @@
 import matplotlib.pyplot as plt
 
-# Input sizes and corresponding execution times (in microseconds)
 input_sizes = [20, 1000, 10000, 20000, 50000, 100000, 200000, 500000]
-execution_times = [0, 0, 35, 91, 654, 2640, 10305, 69709]  # Example times, replace with actual measurements
+execution_times = [0, 0, 35, 91, 654, 2640, 10305, 69709]
 
 plt.plot(input_sizes, execution_times, marker='o')
 plt.title('Input Size vs. Execution Time')
